35f8e08385213e2a7b2d49e636dd4855a8256358
Yujing14 <yuyu1@s.sfusd.edu>
2024-05-23 12:37:52 -0700

    Coyote Jump Timer Try (Fail)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -8,6 +8,7 @@ const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite = $AnimatedSprite2D
+@onready var coyote_jump_timer = $CoyoteJumpTimer
 
 
 func _physics_process(delta):
@@ -42,5 +43,18 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var was_on_floor = is_on_floor()
+	
 	move_and_slide()
+	var just_left_ledge = was_on_floor and not is_on_floor() and velocity.y >= 0
+	if just_left_ledge:
+		coyote_jump_timer.start()
+
+func handle_jump():
+	if is_on_floor() or coyote_jump_timer.time_left > 0.0:
+		if Input.is_action_just_pressed("ui_accept"):
+			velocity.y = JUMP_VELOCITY
+	if not is_on_floor():
+		if Input.is_action_just_released("ui_accept") and velocity.y < JUMP_VELOCITY / 2:
+			velocity.y = JUMP_VELOCITY / 2
+	
