b8627adaf5baaf23ca1ccadf87f225695cd90c14
yaliu10 <yaliu10@s.sfusd.edu>
2024-05-29 09:32:59 -0700

    Added focus on start menu buttons + transition

--- Scripts/level_transition.gd
+++ Scripts/level_transition.gd
@@ -1,23 +1,11 @@
 extends CanvasLayer
 
-signal on_transition_finished
-
 @onready var animation_player = $AnimationPlayer
-@onready var color_rect = $ColorRect
 
-func _reday():
-	color_rect.visible = false
-	animation_player.animation_finished.connect(_on_animation_finished)
-	
+func fade_from_black():
+	animation_player.play("fade_from_black")
+	await animation_player.animation_finished
 
-func _on_animation_finished(anim_name):
-	if anim_name == "fade_to_black":
-		on_transition_finished.emit()
-		animation_player.play("fade_to_normal")
-	elif anim_name =="fade_to_normal":
-		color_rect.visible = false
-
-func transition():
-	color_rect.visible = true
+func fade_to_black():
 	animation_player.play("fade_to_black")
-
+	await animation_player.animation_finished
